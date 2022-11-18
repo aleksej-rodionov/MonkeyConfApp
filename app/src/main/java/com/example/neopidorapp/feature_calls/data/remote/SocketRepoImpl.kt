20ae43bf8c04330e791216e0c0f893710c2b69c6
Aleksej <67003544+aleksej-rodionov@users.noreply.github.com>
2022-11-18 22:27:08 +0600

    TRYING TO UNDERSTAND MVVM+Sockets configuration
    so still in process:
    - initSocket (returning Success/Error)
    - startObservingMessages if Success
    - adjust messageFlow in repo to observe

--- app/src/main/java/com/example/neopidorapp/feature_calls/data/remote/SocketRepoImpl.kt
+++ app/src/main/java/com/example/neopidorapp/feature_calls/data/remote/SocketRepoImpl.kt
@@ -4,6 +4,7 @@ import android.util.Log
 import com.example.neopidorapp.feature_calls.data.remote.model.MessageDto
 import com.example.neopidorapp.feature_calls.domain.model.MessageModel
 import com.example.neopidorapp.shared.Resource
+import com.example.neopidorapp.util.NewMessageInterface
 import com.google.gson.Gson
 import kotlinx.coroutines.flow.*
 import org.java_websocket.client.WebSocketClient
@@ -11,16 +12,16 @@ import org.java_websocket.handshake.ServerHandshake
 import java.net.URI
 
 private const val TAG = "SocketRepoImpl"
-//    private val newMessageInterface: NewMessageInterface
-class SocketRepoImpl(
 
+class SocketRepoImpl(
+//    private val newMessageInterface: NewMessageInterface
 ): SocketRepo {
 
-    private val _incomingMessage: MutableSharedFlow<String> = MutableSharedFlow()
-    private val incomingMessage: SharedFlow<String> = _incomingMessage.asSharedFlow()
-    private suspend fun emitNewMessage(message: String) {
-        _incomingMessage.emit(message)
-    }
+//    private val _incomingMessage: MutableSharedFlow<String> = MutableSharedFlow()
+//    private val incomingMessage: SharedFlow<String> = _incomingMessage.asSharedFlow()
+//    private suspend fun emitNewMessage(message: String) {
+//        _incomingMessage.emit(message)
+//    }
 
     private var webSocket: WebSocketClient? = null
     private var userName: String? = null
@@ -50,8 +51,10 @@ class SocketRepoImpl(
             override fun onMessage(message: String?) {
                 // todo here I want to emit new MessageModel through some new Flow<MessageModel>
                 try {
-
+//                    newMessageInterface.onNewMessage(message)
 //                    incomingMessageFlow(message)
+
+                    incomingMessageFlow().
                 } catch (e: Exception) {
                     e.printStackTrace()
                 }
@@ -81,26 +84,24 @@ class SocketRepoImpl(
         }
     }
 
-    override fun incomingMessageFlow(): Flow<MessageModel> {
-        TODO("Not yet implemented")
-    }
+    override fun incomingMessageFlow(): Flow<MessageModel> = emptyFlow()
 
-//    override fun incomingMessageFlow(messageJson: String?): Flow<MessageModel> = flow {
-//        if (messageJson.isNullOrBlank()) {
-//            // emit nothing
-//            // todo emit Resource.Error after implementing Resource util Sealed Class
-//        } else {
-//            try {
-//                val messageDto = gson.fromJson(messageJson, MessageDto::class.java)
-//                val message = messageDto.toMessageModel()
-//                emit(message)
-//            } catch (e: Exception) {
-//                e.printStackTrace()
-//                // emit nothing
-//                // todo emit Resource.Error after implementing Resource util Sealed Class
-//            }
-//        }
-//    }
+/*    override fun incomingMessageFlow(messageJson: String?): Flow<MessageModel> = flow {
+        if (messageJson.isNullOrBlank()) {
+            // emit nothing
+            // todo emit Resource.Error after implementing Resource util Sealed Class
+        } else {
+            try {
+                val messageDto = gson.fromJson(messageJson, MessageDto::class.java)
+                val message = messageDto.toMessageModel()
+                emit(message)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                // emit nothing
+                // todo emit Resource.Error after implementing Resource util Sealed Class
+            }
+        }
+    }*/
 
     override fun sendMessageToSocket(message: MessageModel) {
         try {
