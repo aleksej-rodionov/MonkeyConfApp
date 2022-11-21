011b6f105889c7c359dd2d63809a552009e4ba55
Aleksej <67003544+aleksej-rodionov@users.noreply.github.com>
2022-11-21 22:23:49 +0600

    RTCNotification.kt class completed
    next:
    - prepare RTCClient to be used inside the service

--- app/src/main/java/com/example/neopidorapp/NeoPidorApp.kt
+++ app/src/main/java/com/example/neopidorapp/NeoPidorApp.kt
@@ -1,7 +1,34 @@
 package com.example.neopidorapp
 
 import android.app.Application
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class NeoPidorApp: Application()
+class NeoPidorApp: Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+
+        createRtcNotificationChannel()
+    }
+
+    private fun createRtcNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val rtcNotificationChannel = NotificationChannel(
+                RTC_NOTIFICATION_CHANNEL_ID,
+                "RTC Notification Channel",
+                NotificationManager.IMPORTANCE_LOW
+            )
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(rtcNotificationChannel)
+        }
+    }
+
+    companion object {
+
+        const val RTC_NOTIFICATION_CHANNEL_ID = "rtcNotificationChannel"
+    }
+}
--- app/src/main/java/com/example/neopidorapp/feature_call/presentation/rtc_service/notification/RTCNotification.kt
+++ app/src/main/java/com/example/neopidorapp/feature_call/presentation/rtc_service/notification/RTCNotification.kt
@@ -2,7 +2,10 @@ package com.example.neopidorapp.feature_call.presentation.rtc_service.notificati
 
 import android.app.PendingIntent
 import android.content.Intent
+import androidx.core.app.NotificationCompat
 import com.example.neopidorapp.MainActivity
+import com.example.neopidorapp.NeoPidorApp
+import com.example.neopidorapp.R
 import com.example.neopidorapp.feature_call.presentation.rtc_service.RTCService
 import com.example.neopidorapp.feature_call.presentation.rtc_service.rtc_ui_state.RTCUiState
 import kotlinx.coroutines.CoroutineScope
@@ -36,6 +39,25 @@ class RTCNotification(
             rtcService, 1, endCallIntent,
             PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
 
-        val notificationBuilder =
+        val notificationBuilder = NotificationCompat.Builder(rtcService, NeoPidorApp.RTC_NOTIFICATION_CHANNEL_ID)
+            .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
+            .setSmallIcon(R.drawable.ic_accept)
+
+        if (state.isOngoingCall) {
+            notificationBuilder.addAction(R.drawable.ic_reject, "End call", endCallPendingIntent)
+        }
+
+        notificationBuilder.setContentIntent(activityPendingIntent)
+
+        val notification = notificationBuilder.setStyle(
+            androidx.media.app.NotificationCompat.MediaStyle().setShowActionsInCompactView())
+//            .setContentTitle(state.playingItem?.title ?: "Unknown mediaItem")
+//            .setContentText(if (state.playingItem?.isBuffering == true) state.playingItem.progress.let { convertTimer(it) } + " ...loading..." // for audioComments
+//            else state.playingItem?.progress?.let { convertTimer(it) } ?: "Unknown progress") // for audioComments
+            .setContentTitle("Ongoing call with ... somebody.")
+            .setContentText(".. is in process") // for audioComments
+            .build()
+
+        rtcService.startForeground(1, notification)
     }
 }
